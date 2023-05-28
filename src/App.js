f44605329689dcc75e1043383a0bef640212ba6d
Grzegorz Zielinski <grzesie2000@gmail.com>
2023-05-28 14:29:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [object, setObject] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/api/flights");
+        setObject(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  const renderObjectValue = (obj) => {
+    return Object.entries(obj).map(([key, value]) => {
+      if (typeof value === "object" && !Array.isArray(value)) {
+        return (
+          <div key={key}>
+            <div style={{ marginLeft: "1rem" }}>{renderObjectValue(value)}</div>
+          </div>
+        );
+      } else {
+        return <div className="object" key={key}>{`${value}`}</div>;
+      }
+    });
+  };
+  // console.log(object);
+  return <div className="object">{renderObjectValue(object)}</div>;
+};
 
 export default App;
